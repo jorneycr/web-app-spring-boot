28e10a13ef3db4ed0449470603a218a55009dd8c
Jorney Lopez <jorney95@live.com>
2021-10-22 16:38:27 -0600

    Fixed tests for project and role

---
+++ src/test/java/com/jdlm/web/app/webapp/RoleJpaTest.java
@@ -0,0 +1,32 @@
+package com.jdlm.web.app.webapp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.jdlm.web.app.webapp.model.Role;
+import com.jdlm.web.app.webapp.repository.IRoleJpaRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class RoleJpaTest {
+    
+    @Autowired
+    private IRoleJpaRepository repoRole;
+
+    @Test
+    public void roleTest(){
+        Role admin = new Role("ROLE_ADMIN");
+        admin = repoRole.save(admin);
+        assertNotNull(admin);
+        assertNotNull(admin.getId());
+        assertEquals("ROLE_ADMIN", admin.getName());
+        assertEquals(admin.getId(), repoRole.findByName("ROLE_ADMIN").getId());
+
+    }
+}
